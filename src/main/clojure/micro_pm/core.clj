a0839e2223e11cc5e793f05b51d36ed5596b68ee
Amit Joshi <ajoshi@netflix.com>
2015-01-12 22:33:03 -0800

    create sprint, assign tasks to sprints

--- src/main/clojure/micro_pm/core.clj
+++ src/main/clojure/micro_pm/core.clj
@@ -1,17 +1,18 @@
 (ns micro-pm.core)
 
 (defrecord User [id name email])
-(defrecord Task [id summary desc category est points status assigned-to])
-(defrecord Sprint [id start end points tasks])
+(defrecord Task [id summary desc category est points status assigned-to sprint-id])
+(defrecord Sprint [id start end points])
 
 (defonce task-id-count (atom 0))
+(defonce sprint-id-count (atom 0))
 (defn get-new-task-id [] (swap! task-id-count inc) @task-id-count)
+(defn get-new-sprint-id [] (swap! sprint-id-count inc) @sprint-id-count)
 
 (defonce app-state
   {:users (ref {})
    :tasks (ref {})
-   :sprints (ref {})
-   :backlog (ref [])})
+   :sprints (ref {})})
 
 (defn add-user
   [user]
@@ -24,7 +25,7 @@
   [{:keys [summary desc category est]}]
   (let [tasks (:tasks app-state)]
     (let [tid (get-new-task-id)
-          task (->Task tid summary desc category est 0 "open" "no-one")]
+          task (->Task tid summary desc category est 0 "open" "no-one" "backlog")]
       (dosync
         (alter tasks assoc tid task)))))
 
@@ -64,6 +65,47 @@
         (alter tasks assoc (:id task)
           (assoc task :status status))))))
 
+(defn create-sprint
+  [start end points]
+  (let [sprint-id (get-new-sprint-id)
+        new-sprint (->Sprint sprint-id start end points)
+        sprints (:sprints app-state)]
+    (dosync
+      (alter sprints assoc sprint-id new-sprint))
+    new-sprint))
+
+(defn sprint-add-task
+  [sprint task]
+  {:pre [(instance? Sprint sprint) (instance? Task task)]}
+  (let [sprints (:sprints app-state)
+        tasks (:tasks app-state)]
+    (when (contains? @sprints (:id sprint))
+      (dosync
+        (alter tasks assoc (:id task)
+          (assoc task :sprint-id (:id sprint)))))))
+
+(defn sprint-remove-task
+  [sprint task]
+  {:pre [(instance? Sprint sprint) (instance? Task task)]}
+  (let [sprints (:sprints app-state)
+        tasks (:tasks app-state)]
+    (when (contains? @sprints (:id sprint))
+      (dosync
+        (alter tasks assoc (:id task)
+          (assoc task :sprint-id "backlog"))))))
+
+(defn get-sprint
+  [sprint-id]
+  (let [sprints (:sprints app-state)]
+    (get @sprints sprint-id)))
+
+(defn tasks-in-sprint
+  [sprint]
+  {:pre [(instance? Sprint sprint)]}
+  (let [sprints (:sprints app-state)
+        tasks (:tasks app-state)]
+    (when (contains? @sprints (:id sprint))
+      (filter #(= (:id sprint) (:sprint-id %)) (vals @tasks)))))
 
 (comment
   (add-user (map->User {:id "amitj" :name "amit"}))
@@ -79,7 +121,16 @@
   (get-user "ddd")
   (get-user "xddd")
   (keys (deref (:tasks app-state)))
+  (keys (deref (:sprints app-state)))
 
-  (get-task 3)
-  (tasks-by-user (get-user "ddd")))
+  (get-task 1)
+  (tasks-by-user (get-user "ddd"))
+  (create-sprint "now" "d+10" 15)
+  (sprint-add-task (get-sprint 2) (get-task 3))
+  (sprint-add-task (get-sprint 2) (get-task 1))
+  (sprint-add-task (get-sprint 2) (get-task 2))
+  (sprint-remove-task (get-sprint 2) (get-task 1))
+  (count (tasks-in-sprint (get-sprint 2)))
+
+  )
 
